ce4e18bb6caea11f62449970176257daffa96251
ITZEL CORTES AGUIRRE <1724110595@utectulancingo.edu.mx>
2026-02-13 14:35:53 +0000

    holiwis

---
+++ API´s/contactos/README.md
@@ -0,0 +1,7 @@
+Para importar datos en sqlite3, te metes a 
+sqlite3 agenda.db
+se cambia el modo a csv con .mode csv
+ponemos .show para ver los datos 
+para importar hacemos .import data.csv y el nombre de la tabla a agregar 
+.import data.csv contactos
+
--- API´s/contactos/main.py
+++ API´s/contactos/main.py
@@ -1,44 +1,81 @@
 from fastapi import FastAPI
-from datetime import datetime
+from fastapi import HTTPException
+from fastapi.responses import JSONResponse
+import sqlite3 as sqlite
 
 app = FastAPI()
 
-@app.get(
-    "/",
-    status_code=202,
-    summary ="Endpoint Raiz",
-    description="Bienvenido a la API de Agenda")
-async def get_root():
+@app.get("/",
+ status_code=202,
+ summary="Endpoint raíz",
+ description= "Bienvenido a la API de agenda")
+def get_root():
     response = {
-        "message": "API de la Agenda",
-        "datetime": "12/02/2026 10:35:45"
+        "message": "API de la agenda",
+        "datatime": "12/02/2026"
         }
     return response
 
-@app.get (
+@app.get(
     "/v1/contactos",
-    status_code= 202,
-    summary = "Endpoint que regresa los contactos paginados",
-    description= """Regresa los contactos paginados,utilizando los siguientes QUERY PARAMS:
-    limit:int -> indica el numero de regsitros a regresar
-    skip:int -> indica el numero de registros a omitir"""
+    status_code=202,
+    summary="Endpoint que ingresa los contactos paginados",
+    description= """Endpoint que ingresa los contactos paginados,
+    utiliza los siguientes query params:
+    limit:int -> Indica el número de registros a regresar
+    skip:int -> Indica el número de registros a omitir"""
 )
-async def get_contactos(skip: int = 40, limit: int = 50):
-    response = {
-        {"table":"contactos",
-         "items": [
-            {"id_contacto": 1,
-            "nombre": "Itzel",
-            "email": "owo@gmail.com",
-            "telefono": "1111111"},
-            {"id_contacto": 2,
-            "nombre": "Chris",
-            "email": "uwu@gmail.com",
-            "telefono": "22222"},
-            ],"count":2,
-            "datetime": "12/02/2026 10:28:2026",
-            "message":"Datos consultados exitosamente",
+async def get_contactos(limit: int = 10, skip: int = 0):
+  #  TODO: Conectar con la base de datos agenda.db
+  #  TODO: Consultar los registros de la tabla contactos
+  #  TODO: Formatear la respuesta con el siguiente schema:
+  #  TODO: Responder la petición
+
+    try:
+        db = sqlite.connect("agenda.db")
+        cursor = db.cursor()
+        cursor.execute("SELECT * FROM contactos LIMIT ? OFFSET ?", (limit, skip))
+        contactos = cursor.fetchall()
+        print(contactos)
+
+        data = {
+            "table": "contactos",
+            "items": [
+                {
+                    "id_contacto":1,
+                    "nombre":"Dejah",
+                    "telefono":"1234567890",
+                    "email":"dejah@email.com"
+                },
+                {
+                    "id_contacto":2,
+                    "nombre":"John Carter",
+                    "telefono":"0987654321",
+                    "email":"john@email.com"    
+                },
+            ],
+            "count": 2,
+            "datetime": "12/02/2026 10:28:30",
+            "message": "Datos consultados exitosamente",
             "limit":limit,
-            "skip":skip}
-    }
-    return response
+            "skip":skip
+        }
+        return JSONResponse(
+            status_code=202,
+            content=data
+        )
+    except Exception as e:
+        print(f"Error al consultar la base de datos: {e.args}")
+        #raise HTTPException(status_code=400, detail="Error al consultar los datos")
+        return JSONResponse(
+            status_code=400,
+            content={
+                "table": "contactos",
+                "items": [],
+                "count": 0,
+                "datetime": "12/02/2026 10:28:30",
+                "message": "Error al consultar los datos",
+                "limit":limit,
+                "skip":skip
+                }
+            )
